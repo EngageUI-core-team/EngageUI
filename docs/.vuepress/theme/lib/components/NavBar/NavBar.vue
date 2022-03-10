e722690375235321411ec7ab283bceb78ff38380
zbd <amarok1217@163.com>
2022-03-10 22:55:00 +0800

    daily

--- docs/.vuepress/theme/lib/components/NavBar/NavBar.vue
+++ docs/.vuepress/theme/lib/components/NavBar/NavBar.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="navBox"></div>
+  <div class="navBox flex-row-center">
+    <div class="title">Engage UI</div>
+  </div>
 </template>
 <script setup lang="ts">
 // import SearchBox from '@SearchBox'
-
 </script>
 <style lang="less" scoped>
-.navBox {
-  width: 100%;
-  height: 60px;
-  background-color: #AD8B73;
-}
+@import url('./NavBar');
+
 </style>
